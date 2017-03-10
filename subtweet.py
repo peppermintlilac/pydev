7b035c5dd50d360dcdd1d7495da48c6f8e8345fe
Cloud User <cloud-user@centos6-1.(none)>
2017-03-10 10:58:58 -0700

    fixed case and formatting; fixed functions

--- subtweet.py
+++ subtweet.py
@@ -1,47 +1,63 @@
 #!/usr/bin/python3
-# work in progress, this doesn't do as it's told
 import twitter
 
+#add keys and token
 c_key = ''
 c_secret = ''
-a_key = ''
+a_key =''
 a_secret = ''
-userwho=''
 
 api = twitter.Api(consumer_key=c_key, consumer_secret=c_secret, access_token_key=a_key, access_token_secret=a_secret)
+#add user
+userwho=""
 
 def VerifyCred():
-    return(api.VerifyCredentials())
+    print(api.VerifyCredentials())
 
 def ShowUserTimeLine():
     statuses = api.GetUserTimeline(screen_name=userwho)
-    return([s.text for s in statuses])
+    print([s.text for s in statuses])
 
 def PostTweet():
-    status=api.PostUpdate('I love python-twitter')
+    asktweet=input("What would you like to tweet today?\n")
+    status=api.PostUpdate(asktweet)
     return(status.text)
 
 def PostLongTweet():
-    longstatus="This tweet is more than one hundred forty characters. The big fox jumps over the lazy dog with the white rabbit. Roses are red. Violets are blue. Posting continuation to next tweet."
-    longpost=api.PostUpdates(longstatus)
+    asklongtweet=input("Fire away...tweet as many words as you can!\n")
+    longpost=api.PostUpdates(asklongtweet)
     return(longpost)
 
 def main():
-    ask=input("What to you want to do? 1: Verify Credentials,\
-            2: Show User Timeline\
-            3: Post a Tweet\
-            4: Post a Long Tweet")
-    answer=int(ask)
+    while True:
+        ask=input("\n\n\
+    \tWhat to you want to do today?\n\
+    \t=============================\n\
+    0: Nothing. Get me out of here!\n\
+    1: Show me my credentials.\n\
+    2: Show me my previous tweets.\n\
+    3: Gonna tweet something.\n\
+    4: I want to post a long tweet. Seriously?\n\n\n")
 
-    if answer is 1:
-        VerifyCred
-    elif answer is 2:
-        ShowUserTimeLine
-    elif answer is 3:
-        PostTweet
-    elif answer is 4:
-        PostLongTweet
-    else:
-        print("Invalid option")
+        answer=int(ask)
+
+        if answer is 1:
+            VerifyCred()
+
+        elif answer is 2:
+            ShowUserTimeLine()
+
+        elif answer is 3:
+            PostTweet()
+
+        elif answer is 4:
+            PostLongTweet()
+
+        elif answer is 0:
+            break
+            return()
+
+        else:
+            print("Invalid option. Choose between 1 thru 4.\n")
 
 if __name__ == "__main__": main()
