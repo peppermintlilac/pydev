fa7fd49c20c56f231fab8996dcb286eb75b5e842
Cloud User <cloud-user@centos6-1.(none)>
2017-03-10 12:53:11 -0700

    learning array functionality

--- high_score.py
+++ high_score.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
 highest_score = 0
+scores = []
 
 result_f = open("results.txt")
 for line in result_f:
     (name,score) = line.split()
-    if float(score) > highest_score:
-        highest_score = float(score)
+    scores.append(float(score))
+
 result_f.close()
-print("The highest score was:")
-print(highest_score)
+print("The highest scores are:")
+print(scores[0])
+print(scores[1])
+print(scores[2])
