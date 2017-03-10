20a9be24cf249400988fd68b9f0c00468fb85f53
Cloud User <cloud-user@centos6-1.(none)>
2017-03-10 12:21:57 -0700

    learning split functionality

---
+++ high_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+highest_score = 0
+
+result_f = open("results.txt")
+for line in result_f:
+    (name,score) = line.split()
+    if float(score) > highest_score:
+        highest_score = float(score)
+result_f.close()
+print("The highest score was:")
+print(highest_score)
